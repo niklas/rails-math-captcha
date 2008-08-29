3765e23b8878498baf098d8eb7dda53f35a2ef11
Randy Schmidt <randy@umlatte.com>
2008-08-28 22:37:36 -0400

    Make the captcha check work

--- lib/math_captcha/has_captcha.rb
+++ lib/math_captcha/has_captcha.rb
@@ -1,14 +1,13 @@
 module MathCaptcha
   module HasCaptcha
-    def check_captcha
-      self.errors.add_to_base("Captcha not answered correctly.") unless self.captcha.check(self.captcha_solution)
+    def validate_on_create
+      self.errors.add(:captcha_solution, "wrong answer.") unless self.captcha.check(self.captcha_solution)
     end
     
     module ClassMethods
       def has_captcha
         attr_accessor :captcha, :captcha_solution
         validates_presence_of :captcha_solution, :on => :create, :message => "can't be blank"
-        before_validation :check_captcha
       end
     end
   end
