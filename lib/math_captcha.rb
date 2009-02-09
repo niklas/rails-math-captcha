2ae6dd2de5f2e7a185e5c69d7cb19f9ef95e1140
Niklas Hofer <niklas+dev@lanpartei.de>
2009-02-09 16:16:14 +0100

    no before_filter. everything happens in the model

--- lib/math_captcha.rb
+++ lib/math_captcha.rb
@@ -1,3 +1,2 @@
 require 'math_captcha/captcha'
-require 'math_captcha/before_filters'
 require 'math_captcha/has_captcha'
--- lib/math_captcha/before_filters.rb
+++
@@ -1,9 +0,0 @@
-module MathCaptcha
-  module BeforeFilters
-    def set_captcha
-      session[:captcha] = Captcha.new
-    end
-  end
-end
-
-ActionController::Base.send(:include, MathCaptcha::BeforeFilters)
